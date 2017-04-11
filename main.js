bb0f176bef0e7e8bc9c6ce0a944ba123af293a1e
Nick Felker <handnf@gmail.com>
2017-04-11 13:20:04 -0400

    Simple round-robin process system

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
-(function() {
-    // Booting
+// Kernel
 
-    setTimeout(function() {
-//        $('#splashscreen').style.display = 'none';
-    }, 1000);
+function boot_state(txt) {
+    console.log(txt);
+    $('#boot_status').innerHTML = txt;
+}
+
+function delay(ms) {
+    var time = new Date().getTime();
+    while (new Date().getTime() - time < ms) {}
+}
+
+(function() {
+     var init_process = new Promise(function(fulfill, reject) {
+        // Schedule idle task
+        var process_table = [];
+        // As JS passes by reference, we can create the ptable here and pass it into the process manager
+        process_init(process_table);
+        function task_scheduler() {
+            // Iterate through every process and execute it.
+            // Simple round robin implementation. No priority.
+            for (i in process_table) {
+                var p = process_table[i];
+                process_exec(p);
+            }
+            // Now set up another system call in a second - This is the null process
+            setTimeout(task_scheduler, PROCESS_TIMING_QUANTUM);
+        }
+
+         function idle() {
+             // Don't do anything
+         }
+
+         process_add("System Idle Process", idle, 'process_table');
+         setTimeout(task_scheduler, PROCESS_TIMING_QUANTUM);
+         fulfill();
+    });
+
+    var init_dream_journal = new Promise(function(fulfill, reject) {
+        fulfill();
+    })
+
+    boot_state("Loading...");
+    init_process.then(function() {
+        boot_state("Restarting tasks...");
+        init_dream_journal.then(function() {
+            setTimeout(function() {
+                $('#splashscreen').style.display = 'none';
+            }, 2000);
+        });
+    });
 })();
--- process.js
+++ process.js
@@ -1,6 +1,63 @@
 const PROCESS_STATE_WAITING = 1;
 const PROCESS_STATE_READY = 2;
+const PROCESS_STATE_ACTIVE = 3;
+
+const PROCESS_TIMING_QUANTUM = 1000; // Milliseconds
+const PROCESS_MAX = 1000; // For over this number of processes, the system cannot generate a uniuqe id
+
+// Define function headers
+
+// Inits the process system
+process_init = null;
+// Allows a process to be created and added to the task scheduler.
+process_add = null;
+// Runs a process once
+process_exec = null;
 
 (function() {
+    const PTABLE_COLUMN_PID = "pid";
+    const PTABLE_COLUMN_NAME = "name";
+    const PTABLE_COLUMN_FNC = "function";
+    const PTABLE_COLUMN_STATE = "state";
+    const PTABLE_COLUMN_BASE_REGISTER = "base_reg";
+    const PTABLE_COLUMN_LIMIT_REGISTER = "lim_reg";
 
+    var is_process_init = false;
+
+    var process_table = undefined;
+
+    process_init = function(table) {
+        if (!is_process_init) {
+            process_table = table;
+            is_process_init = true; // Only allows process init to occur first time
+        }
+    }
+
+    process_add = function(name, process) {
+        // Generate process
+        var p = {};
+        p[PTABLE_COLUMN_NAME] = name;
+        p[PTABLE_COLUMN_FNC] = process;
+        // Default
+        p[PTABLE_COLUMN_PID] = process_generate_id();
+        p[PTABLE_COLUMN_STATE] = PROCESS_STATE_READY;
+        process_table[p[PTABLE_COLUMN_PID]] = p;
+    }
+
+    process_exec = function(process) {
+        // Set state to active
+        process[PTABLE_COLUMN_STATE] = PROCESS_STATE_ACTIVE;
+        process[PTABLE_COLUMN_FNC]();
+        // Put back into waiting
+        process[PTABLE_COLUMN_STATE] = PROCESS_STATE_READY;
+    }
+
+    function process_generate_id() {
+        var pid = -1;
+        while (pid == -1 || process_table[pid] != undefined) {
+            pid = Math.round((Math.random() * PROCESS_MAX));
+            console.log(pid);
+        }
+        return pid;
+    }
 })();
